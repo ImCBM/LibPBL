e10f5a9e63692c8cf212b9ac169821727c57dcc0
Sai <Sai@SAIDENCITY>
2024-01-08 03:09:02 +0800

    Make the LibrarianForm first numbnut

---
+++ LibPBL/MainLibrary.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace LibPBL
+{
+    partial class MainLibrary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlLeft = new Panel();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            pictureBox1 = new PictureBox();
+            pnlTopHeader = new Panel();
+            pnlLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pnlLeft
+            // 
+            pnlLeft.BackColor = Color.FromArgb(15, 33, 103);
+            pnlLeft.Controls.Add(iconButton1);
+            pnlLeft.Controls.Add(pictureBox1);
+            pnlLeft.Dock = DockStyle.Left;
+            pnlLeft.Location = new Point(0, 0);
+            pnlLeft.Name = "pnlLeft";
+            pnlLeft.Size = new Size(235, 681);
+            pnlLeft.TabIndex = 0;
+            // 
+            // iconButton1
+            // 
+            iconButton1.FlatStyle = FlatStyle.Flat;
+            iconButton1.Font = new Font("Arial Black", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            iconButton1.ForeColor = Color.FromArgb(255, 236, 214);
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.None;
+            iconButton1.IconColor = Color.Black;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.Location = new Point(0, 195);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(232, 54);
+            iconButton1.TabIndex = 1;
+            iconButton1.Text = "View Books";
+            iconButton1.TextAlign = ContentAlignment.MiddleRight;
+            iconButton1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Top;
+            pictureBox1.Image = Properties.Resources.LibLogo;
+            pictureBox1.InitialImage = null;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(235, 163);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pnlTopHeader
+            // 
+            pnlTopHeader.BackColor = Color.FromArgb(15, 33, 103);
+            pnlTopHeader.Dock = DockStyle.Top;
+            pnlTopHeader.Location = new Point(235, 0);
+            pnlTopHeader.Name = "pnlTopHeader";
+            pnlTopHeader.Size = new Size(1029, 100);
+            pnlTopHeader.TabIndex = 1;
+            // 
+            // MainLibrary
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1264, 681);
+            Controls.Add(pnlTopHeader);
+            Controls.Add(pnlLeft);
+            MaximumSize = new Size(1280, 720);
+            MinimumSize = new Size(1280, 720);
+            Name = "MainLibrary";
+            Text = "MainLibrary";
+            Load += MainLibrary_Load;
+            pnlLeft.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlLeft;
+        private Panel pnlTopHeader;
+        private PictureBox pictureBox1;
+        private FontAwesome.Sharp.IconButton iconButton1;
+    }
+}
---
+++ LibPBL/MainLibrary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibPBL
+{
+    public partial class MainLibrary : Form
+    {
+        private string LogInID;
+        private string variable2;
+        public MainLibrary(string var1, string var2)
+        {
+            InitializeComponent();
+            LogInID = var1;
+            variable2 = var2;
+        }
+
+        private void MainLibrary_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
